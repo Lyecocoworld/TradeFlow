719317038175f6ec4a2d065aab8527573feefe5c
Lye <raphael.berger@epita.fr>
2026-04-20 15:14:13 +0200

    Delete list_jar.ps1

--- list_jar.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-$jarPath = 'C:\Users\space\.gradle\caches\modules-2\files-2.1\fr.maxlego08.menu\zmenu-api\1.1.1.2\44a4b0159ab0cda1522aba3d94aa0eb5d89269fb\zmenu-api-1.1.1.2.jar'
-$zip = [System.IO.Compression.ZipFile]::OpenRead($jarPath)
-foreach ($entry in $zip.Entries) {
-    Write-Output $entry.FullName
-}
-$zip.Dispose()
